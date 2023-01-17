96d3ffb954ea193c3fb79ea865ca5800521ee424
jonesnxt <jones25519@gmail.com>
2023-01-17 00:49:55 -0800

    okay so I'm a little tired

--- src/util/getOauthURL.ts
+++ src/util/getOauthURL.ts
@@ -1,5 +1,5 @@
 const GITHUB_CLIENT_ID = '726814fd13cb9d417f4b';
 
 export default function getOauthURL(repo: string) {
-  return `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=repo&type=user_agent&redirect_uri=https://stats.subsecond.dev/${repo}`;
+  return `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=repo&type=user_agent&redirect_uri=https://stats.subsecond.app/${repo}`;
 }
